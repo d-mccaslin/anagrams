3fa2890ea946b37457480581dee7725b2139c53f
David McCaslin <davidmccaslin94@gmail.com>
2020-09-25 15:57:26 -0700

    refactor code to include custom class (Phrase) with instance variables (word1 & word2) and instance method (anagrams)

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -1,33 +1,42 @@
 require('pry')
 
-def anagrams(word1,word2)
-  word1 = word1.downcase.gsub(/\W/,'')
-  word2 = word2.downcase.gsub(/\W/,'')
-
-  if !(word1.match(/[aeiou]/))
-    return "Input real words"
+class Phrase
+  def initialize(word1,word2)
+    @word1 = word1
+    @word2 = word2
   end
 
-  if !(word2.match(/[aeiou]/))
-    return "Input real words"
-  end
+  def anagrams
+    word1 = @word1
+    word2 = @word2
 
-  word_array_1 = word1.split('')
+    word1 = word1.downcase.gsub(/\W/,'')
+    word2 = word2.downcase.gsub(/\W/,'')
 
-  matches = 0
-
-  word_array_1.each do |letter_1|
-    if word2.include? letter_1
-      matches += 1
+    if !(word1.match(/[aeiou]/))
+      return "Input real words"
     end
-  end
 
-  if matches == word2.length
-    return "These words are anagrams"
-  elsif matches = 0
-    return "These words are antigrams"
-  else
-    return "These words are not anagrams"
+    if !(word2.match(/[aeiou]/))
+      return "Input real words"
+    end
+
+    word_array_1 = word1.split('')
+    matches = 0
+
+    word_array_1.each do |letter_1|
+      if word2.include? letter_1
+        matches += 1
+      end
+    end
+
+    if matches == word2.length
+      return "These words are anagrams"
+    elsif matches = 0
+      return "These words are antigrams"
+    else
+      return "These words are not anagrams"
+    end
   end
 
 end

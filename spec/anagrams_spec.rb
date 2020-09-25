22cc9caba77fe65f345c5b5175b4cd7add2817ee
David McCaslin <davidmccaslin94@gmail.com>
2020-09-25 13:52:30 -0700

    add test spec to require words to contain vowels to qualify as anagram

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,18 +1,22 @@
 require('rspec')
 require('anagrams')
 
-describe("String#anagram") do
+describe("#isAnagram") do
 
   # it("returns a string") do
   #   expect("hello".anagram).to(eq("hello"))
   # end
 
   it("checks if two words are anagrams") do
-    expect(anagram("ruby","bury")).to(eq(true))
+    expect(isAnagram("ruby","bury")).to(eq(true))
   end
 
   it("accounts for differences in case between the words") do
-    expect(anagram("Eat","Tea")).to(eq(true))
+    expect(isAnagram("Eat","Tea")).to(eq(true))
+  end
+
+  it("requires both inputs to be 'real' words, meaning they each contain a vowel") do
+    expect(isAnagram("rbb","brb")).to(eq(false))
   end
 
 end

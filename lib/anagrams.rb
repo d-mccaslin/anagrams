75b19126f96f119711da3d7f9012430f9249849d
David McCaslin <davidmccaslin94@gmail.com>
2020-09-25 13:40:23 -0700

    update method to pass spec

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -2,5 +2,22 @@ require('pry')
 
 def anagram(word1,word2)
   puts word1, word2
-  return true
+
+  word_array_1 = word1.split('')
+  #word_array_2 = word2.split('')
+
+  matches = 0
+
+  word_array_1.each do |letter_1|
+    if word2.include? letter_1
+      matches += 1
+    end
+  end
+
+  if matches == word2.length
+    return true
+  else
+    return false
+  end
+
 end

26e426b82778a72e3af0181832753e32e6d82f12
David McCaslin <davidmccaslin94@gmail.com>
2020-09-25 15:58:01 -0700

    update specs to class + method

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,30 +1,31 @@
 require('rspec')
 require('anagrams')
 
-describe("#anagrams") do
-
-  # it("returns a string") do
-  #   expect("hello".anagram).to(eq("hello"))
-  # end
+describe("Phrase#anagrams") do
 
   it("checks if two words are anagrams") do
-    expect(anagrams("ruby","bury")).to(eq("These words are anagrams"))
+    phrase = Phrase.new("ruby","bury")
+    expect(phrase.anagrams).to(eq("These words are anagrams"))
   end
 
   it("accounts for differences in case between the words") do
-    expect(anagrams("Eat","Tea")).to(eq("These words are anagrams"))
+    phrase = Phrase.new("Eat","Tea")
+    expect(phrase.anagrams).to(eq("These words are anagrams"))
   end
 
   it("requires both inputs to be 'real' words, meaning they each contain a vowel") do
-    expect(anagrams("rbb","brb")).to(eq("Input real words"))
+    phrase = Phrase.new("rbb","brb")
+    expect(phrase.anagrams).to(eq("Input real words"))
   end
 
   it("checks if the words are antigrams, meaning that no letters match") do
-    expect(anagrams("car","bus")).to(eq("These words are antigrams"))
+    phrase = Phrase.new("car","bus")
+    expect(phrase.anagrams).to(eq("These words are antigrams"))
   end
 
   it("accounts for multi-word and punctuated phrases being anagrams or antigrams") do
-    expect(anagrams("Hello, team.","Elloh mate!")).to(eq("These words are anagrams"))
+    phrase = Phrase.new("Hello, team.","Elloh mate!")
+    expect(phrase.anagrams).to(eq("These words are anagrams"))
   end
 
 end

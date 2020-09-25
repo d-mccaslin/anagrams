8fdb225373932a9324e4685d076cd46ef52ccf33
David McCaslin <davidmccaslin94@gmail.com>
2020-09-25 10:33:22 -0700

    add first real spec

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -3,8 +3,12 @@ require('anagrams')
 
 describe("String#anagram") do
 
-  it("returns a string") do
-    expect("hello".anagram).to(eq("hello"))
+  # it("returns a string") do
+  #   expect("hello".anagram).to(eq("hello"))
+  # end
+
+  it("checks if two words are anagrams") do
+    expect(anagram("ruby","bury")).to(eq(true))
   end
 
 end

dc15b70401581b1badc1c7a3156fc30774f48b9c
David McCaslin <davidmccaslin94@gmail.com>
2020-10-05 07:59:30 -0700

    update script file to take user input and return proper result

--- user_script.rb
+++ user_script.rb
@@ -1,3 +1,15 @@
 #!/usr/bin/ruby
 
 require('./lib/anagrams.rb')
+
+puts "Please enter the first word for comparison:"
+word1 = gets.chomp
+
+puts "Please enter the second word for comparison"
+word2 = gets.chomp
+
+comparison = Phrase.new(word1,word2)
+
+result = comparison.anagrams
+
+puts result
